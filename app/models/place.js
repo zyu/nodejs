c28158d86f9400ae8b6fefbd15e6e51c3d87c858
zyuvszqk <zyuvszqk@gmail.com>
2015-05-28 13:32:41 +0800

    Revert "amap weixin"
    This reverts commit 54120bd58749dcb1c7c8e398ebe7074c7f2a0d35.

--- app/models/place.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose')
-var PlaceSchema = require('../schemas/place')
-var Place = mongoose.model('Place', PlaceSchema)
-PlaceSchema.index({ location: '2dsphere' });
-module.exports = Place
